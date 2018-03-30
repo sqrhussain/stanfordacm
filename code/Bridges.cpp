63e6f01347d602d9c0429780dae9f72a1a1beb50
Hussain Hussain <hussain@student.tugraz.at>
2018-03-31 01:27:37 +0200

    edited style and removed annoying spaces

--- code/Bridges.cpp
+++ code/Bridges.cpp
@@ -1,24 +1,21 @@
-#include<bits/stdc++.h>
-using namespace std;
 
-const int N = 1e5;
 int dfslow[N];
 int dfsnum[N];
 int dfscnt = 1;
 vector<int> adj[N];
 void dfs(int u, int p) {
-	dfslow[u] = dfsnum[u] = dfscnt++;
-	for (int i = 0; i < adj[u].size(); i++) {
-		int v = adj[u][i];
-		if (!dfsnum[v]) {
-			dfs(v, u);
-			if(dfslow[v]>dfsnum[u]){
-				//it's a bridge
-			}
-			dfslow[u]=min(dfslow[u],dfslow[v]);
-		}else if(v!=p){
-			//back edge
-			dfslow[u]=min(dfslow[u],dfsnum[v]);
-		}
-	}
+  dfslow[u] = dfsnum[u] = dfscnt++;
+  for (int i = 0; i < adj[u].size(); i++) {
+    int v = adj[u][i];
+    if (!dfsnum[v]) {
+      dfs(v, u);
+      if(dfslow[v]>dfsnum[u]){
+        //it's a bridge
+      }
+      dfslow[u]=min(dfslow[u],dfslow[v]);
+    }else if(v!=p){
+      //back edge
+      dfslow[u]=min(dfslow[u],dfsnum[v]);
+    }
+  }
 }
