8b704634edeff1543db38bc529fdf0ae59c88cac
Hussain Hussain <hussain@student.tugraz.at>
2018-04-10 16:08:47 +0200

    fixing watevs

--- code/BIT.cc
+++ code/BIT.cc
@@ -1,28 +1,8 @@
-#include <iostream>
-using namespace std;
 
 #define LOGSZ 17
 
 int tree[(1<<LOGSZ)+1];
 int N = (1<<LOGSZ);
-
-// add v to value at x
-void set(int x, int v) {
-  while(x <= N) {
-    tree[x] += v;
-    x += (x & -x);
-  }
-}
-
-// get cumulative sum up to and including x
-int get(int x) {
-  int res = 0;
-  while(x) {
-    res += tree[x];
-    x -= (x & -x);
-  }
-  return res;
-}
 
 // get largest value with cumulative sum less than or equal to x;
 // for smallest, pass x-1 and add 1 to result
